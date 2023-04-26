e41f051394032b2583602cc19b9388148a95e4fd
nesmun <jesus.emc@hotmail.com>
2023-04-26 14:38:06 -0600

    06done

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+
+export default function Cards({ characters }) {
+  const onClose = () => {
+    window.alert("Cerrando tarjeta");
+  };
+  return (
+    <div>
+      {characters.map(({ name, species, gender, image }) => {
+        return (
+          <Card
+            name={name}
+            species={species}
+            gender={gender}
+            image={image}
+            onClose={onClose}
+          />
+        );
+      })}
+    </div>
+  );
+}
