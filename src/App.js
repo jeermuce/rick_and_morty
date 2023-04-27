eb36f9d508152a22c59f9af64bd1af9c9008c4d6
nesmun <jesus.emc@hotmail.com>
2023-04-27 03:56:11 -0600

    .

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Cards from "./components/Cards/Cards.jsx";
 import SearchBar from "./components/SearcBar/SearchBar";
 import characters from "./data.js";
 import style from "./App.module.css";
+import backgroundImage from "./portal.png";
+
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <div className={style.nav}>
         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
       </div>
