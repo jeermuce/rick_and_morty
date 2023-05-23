eb986647741ef8a05e618751c7afc831619ef1ee
nesmun <jesus.emc@hotmail.com>
2023-05-23 14:07:15 -0600

    return to monke

--- Front/src/components/About/About.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div>
-      <h1>My name is Jeff</h1>
-    </div>
-  );
-}
---
+++ Front/trial.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Only change code below this line
+
+  name: "",
+  legs: 1,
+  tails: 57,
+  friends: [],
+  // Only change code above this line
+};
