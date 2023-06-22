fa14f3c36f168fbbfe49e874fd388951979a3c9a
nesmun <jesus.emc@hotmail.com>
2023-06-22 06:27:14 -0600

    stuff

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -5,65 +5,69 @@ import style from "./Form.module.css";
 import Portal from "../../assets/portal_best.png";
 
 const Form = ({ login, setAccess }) => {
-  const [userData, setUserData] = useState({ email: "", password: "" });
-  const [errors, setErrors] = useState({ email: "", password: "" });
+    const [userData, setUserData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({ email: "", password: "" });
 
-  useEffect(() => {
-    setAccess(false);
-  }, []);
+    useEffect(() => {
+        setAccess(false);
+    }, []);
 
-  function handleInputChange(event) {
-    setUserData({ ...userData, [event.target.name]: event.target.value });
-    validation(
-      { ...userData, [event.target.name]: event.target.value },
-      errors,
-      setErrors
+    function handleInputChange(event) {
+        setUserData({ ...userData, [event.target.name]: event.target.value });
+        validation(
+            { ...userData, [event.target.name]: event.target.value },
+            errors,
+            setErrors
+        );
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        login(userData);
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className={style.Form}>
+            <section className={style.imageContainer}>
+                <img src={Portal} alt="Portal" className={style.portalImage} />
+            </section>
+            <section className={style.formContainer}>
+                <section className={style.inputContainer}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="email"
+                        name="email"
+                        className={style.formInput}
+                        value={userData.email}
+                        onChange={handleInputChange}
+                        placeholder="email, 35 char max..."
+                    />
+
+                    <p>{errors.username}</p>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className={style.formInput}
+                        placeholder="6-10 chars; A0a-Z9z..."
+                        value={userData.password}
+                        onChange={handleInputChange}
+                    />
+
+                    <p>{errors.password}</p>
+                    <div className={style.loginDiv}>
+                        <button
+                            type="submit"
+                            className={style.loginButton}
+                            onTouchStart={handleSubmit}
+                        >
+                            Login
+                        </button>
+                    </div>
+                </section>
+            </section>
+        </form>
     );
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    login(userData);
-  }
-
-  return (
-    <form onSubmit={handleSubmit} className={style.Form}>
-      <section className={style.imageContainer}>
-        <img src={Portal} alt="Portal" className={style.portalImage} />
-      </section>
-      <section className={style.formContainer}>
-        <section className={style.inputContainer}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="email"
-            name="email"
-            className={style.formInput}
-            value={userData.email}
-            onChange={handleInputChange}
-            placeholder="email, 35 char max..."
-          />
-
-          <p>{errors.username}</p>
-          <label htmlFor="password">Password</label>
-          <input
-            type="text"
-            name="password"
-            className={style.formInput}
-            placeholder="6-10 chars; A0a-Z9z..."
-            value={userData.password}
-            onChange={handleInputChange}
-          />
-
-          <p>{errors.password}</p>
-          <div className={style.loginDiv}>
-            <button type="submit" className={style.loginButton}>
-              Login
-            </button>
-          </div>
-        </section>
-      </section>
-    </form>
-  );
 };
 
 export default Form;
--- server/src/utils/users.js
+++ server/src/utils/users.js
@@ -1,3 +1,4 @@
 const EMAIL = "Rick@schwifty.com";
 const PASSWORD = "M59tw123";
+
 module.exports = [{ email: EMAIL, password: PASSWORD }];
