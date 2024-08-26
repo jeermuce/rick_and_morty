d5ec7b80b43e78f1b9e6602f86ff799d3b0efa60
jeermuce <ernesto_mc.dev@proton.me>
2024-08-26 15:27:30 -0600

    added ssl

--- server/src/DB_connection.js
+++ server/src/DB_connection.js
@@ -9,19 +9,19 @@ const UserModel = require("./models/User");
 // Recuerda pasarle la información de tu archivo '.env'.
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    { logging: false, native: false }
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+	{ logging: false, native: false, dialectOptions: { ssl: true } },
 );
 console.log("Calling authenticate...");
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Connection has been established successfully.");
-    })
-    .catch((err) => {
-        console.error("Unable to connect to the database:", err);
-    });
+	.authenticate()
+	.then(() => {
+		console.log("Connection has been established successfully.");
+	})
+	.catch((err) => {
+		console.error("Unable to connect to the database:", err);
+	});
 
 // EJERCICIO 05
 // Debajo de este comentario puedes ejecutar la función de los modelos.
@@ -35,15 +35,15 @@ UserModel(sequelize);
 // ¡Relaciona tus modelos aquí abajo!
 const { User, Favorite } = sequelize.models;
 User.belongsToMany(Favorite, {
-    through: "user_favorite",
-    timestamps: false,
+	through: "user_favorite",
+	timestamps: false,
 });
 Favorite.belongsToMany(User, {
-    through: "user_favorite",
-    timestamps: false,
+	through: "user_favorite",
+	timestamps: false,
 });
 module.exports = {
-    User,
-    Favorite,
-    conn: sequelize,
+	User,
+	Favorite,
+	conn: sequelize,
 };
