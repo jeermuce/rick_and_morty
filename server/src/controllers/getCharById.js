725f16ac7e230b0c824b3f5c6bda4b2e61e3d241
jeermuce <ernesto_mc.dev@proton.me>
2024-08-26 14:57:07 -0600

    Formatting, remove .env

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -1,27 +1,28 @@
 const URL = "https://rickandmortyapi.com/api/character";
 const axios = require("axios");
 async function getCharById(req, res) {
-    try {
-        const { param_id } = req.params;
-        const { data } = await axios.get(`${URL}/${param_id}`);
-        const { id, status, name, species, origin, image, gender } = data;
-        if (!data.name)
-            throw new Error(`Missing character data of character ${id}`);
-        const character = {
-            id,
-            status,
-            name,
-            species,
-            origin,
-            image,
-            gender,
-        };
-        return res.status(200).json(character);
-    } catch (error) {
-        error.message.includes("ID")
-            ? res.status(404).send({ error: error.message })
-            : res.status(500).send({ error: error.response.data.error });
-    }
+	try {
+		const { param_id } = req.params;
+		const { data } = await axios.get(`${URL}/${param_id}`);
+		const { id, status, name, species, origin, image, gender } = data;
+		if (!data.name)
+			throw new Error(`Missing character data of character ${id}`);
+		const character = {
+			id,
+			status,
+			name,
+			species,
+			origin,
+			image,
+			gender,
+		};
+		console.log(character);
+		return res.status(200).json(character);
+	} catch (error) {
+		error.message.includes("ID")
+			? res.status(404).send({ error: error.message })
+			: res.status(500).send({ error: error.response.data.error });
+	}
 }
 
 module.exports = getCharById;
